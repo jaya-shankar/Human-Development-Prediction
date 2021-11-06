170b37d53470cb4b42da9a40e2ed139158c78330
jaya-shankar <bjayashankar2102@gmail.com>
2021-11-06 17:46:48 +0530

    add data cleaning code

---
+++ data_extract_code/population.py
@@ -0,0 +1,33 @@
+import csv
+
+file  = open("population.csv")
+
+csvreader = csv.reader(file)
+
+header = next(csvreader)
+l = len(header)
+
+with open('converted_pop.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+    # write the data
+    for row in csvreader:
+        s=[row[0]]
+
+        for e in range(1,l):
+            m=1
+            if(row[e][-1]=='k'):
+                m=1000
+                s.append(str(int(float(row[e][:-1])*m)))
+                # print(int(float(row[e][:-1])*m))
+            elif(row[e][-1]=='M'):
+                m=1000000
+                s.append(str(int(float(row[e][:-1])*m)))
+                # print(int(float(row[e][:-1])*m))
+            else:
+                s.append(row[e])
+            
+        writer.writerow(s)
+    
