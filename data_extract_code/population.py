f0d2a404baa9a4354798801b6223739a4d136d7a
jaya-shankar <bjayashankar2102@gmail.com>
2022-03-25 09:31:17 +0530

    add le and gdp_per_capita new datasets

--- data_extract_code/population.py
+++ data_extract_code/population.py
@@ -1,13 +1,13 @@
 import csv
 
-file  = open("../datasets/converted_pop.csv")
+file  = open("../datasets/gdppercapita_us_inflation_adjusted.csv")
 
 csvreader = csv.reader(file)
 
 header = next(csvreader)
 l = len(header)
 
-with open('converted_pop_b.csv', 'w', encoding='UTF8') as f:
+with open('gdppercapita_us_inflation_adjusted_c.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
 
     # write the header
@@ -18,18 +18,21 @@ with open('converted_pop_b.csv', 'w', encoding='UTF8') as f:
 
         for e in range(1,l):
             m=1
-            if(row[e][-1]=='k'):
-                m=1000
-                s.append(str(int(float(row[e][:-1])*m)))
-                # print(int(float(row[e][:-1])*m))
-            elif(row[e][-1]=='M'):
-                m=1000000
-                s.append(str(int(float(row[e][:-1])*m)))
-                # print(int(float(row[e][:-1])*m))
-            elif(row[e][-1]=='B'):
-                m=1000000000
-                s.append(str(int(float(row[e][:-1])*m)))
-                # print(int(float(row[e][:-1])*m))
+            if row[e]!="":
+                if(row[e][-1]=='k'):
+                    m=1000
+                    s.append(str(int(float(row[e][:-1])*m)))
+                    # print(int(float(row[e][:-1])*m))
+                elif(row[e][-1]=='M'):
+                    m=1000000
+                    s.append(str(int(float(row[e][:-1])*m)))
+                    # print(int(float(row[e][:-1])*m))
+                elif(row[e][-1]=='B'):
+                    m=1000000000
+                    s.append(str(int(float(row[e][:-1])*m)))
+                    # print(int(float(row[e][:-1])*m))
+                else:
+                    s.append(row[e])
             else:
                 s.append(row[e])
             
